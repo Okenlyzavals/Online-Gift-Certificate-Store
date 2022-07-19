31f34da2e3a374f15482fb8983d0180577197457
YauheniBaranouski1 <yauheni_baranouski1@epam.com>
2022-07-19 16:07:47 +0300

    Prepared for CI/CD

--- web/src/main/java/com/epam/ems/web/SpringBootApp.java
+++ web/src/main/java/com/epam/ems/web/SpringBootApp.java
@@ -1,45 +1,28 @@
 package com.epam.ems.web;
 
-import com.epam.ems.service.DatasourceFiller;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.core.env.Environment;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication(scanBasePackages = {"com.epam.ems"})
 @EnableSpringDataWebSupport
 @EnableJpaRepositories("com.epam.ems")
 @EntityScan("com.epam.ems")
 @Component
-public class SpringBootApp {
-
-    private final DatasourceFiller filler;
-    private final Environment environment;
-    @Autowired
-    PasswordEncoder encoder;
-
-    @Autowired
-    public SpringBootApp(DatasourceFiller filler, Environment environment) {
-        this.filler = filler;
-        this.environment = environment;
-    }
+public class SpringBootApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class,args);
     }
 
-    @PostConstruct
-    public void fillDataSource(){
-        if (Boolean.TRUE.equals(Boolean.valueOf(environment.getProperty("certificates.fillDB")))){
-            filler.fillSourceWithData();
-        }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootApp.class);
     }
 
 }
